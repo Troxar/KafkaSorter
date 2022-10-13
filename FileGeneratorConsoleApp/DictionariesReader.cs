43b87a0ebeff79a117af8bb58bbcc58008bf2532
Alexander Katkov <afargoth@gmail.com>
2022-10-13 20:58:06 +0700

    Read words from dictionaries

---
+++ FileGeneratorConsoleApp/DictionariesReader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace FileGeneratorConsoleApp
+{
+    internal class DictionariesReader
+    {
+        private readonly string _folderPath;
+
+        internal DictionariesReader(string folderPath)
+        {
+            _folderPath = folderPath;
+        }
+        
+        internal string[] Read()
+        {
+            var files = Directory.EnumerateFiles(_folderPath).ToArray();
+            var lines = files.AsParallel()
+                .Select(File.ReadLines)
+                .SelectMany(x => x)
+                .ToArray();
+            return lines;
+        }
+    }
+}
---
+++ FileGeneratorConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+
+namespace FileGeneratorConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Dictionaries reading...");
+            
+            var folderPath = ConfigurationManager.AppSettings.Get("DictionariesPath");
+            var reader = new DictionariesReader(folderPath);
+            var words = reader.Read();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
